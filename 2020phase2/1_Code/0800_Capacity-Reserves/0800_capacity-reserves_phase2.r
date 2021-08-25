de464f7685afd6b587aeb6af2353fbc6d87698eb
Rosin Klemens (SSZ) <sszrok@global.szh.loc>
2021-08-25 11:56:40 +0200

    new code (capacity/reserves)

--- 2020phase2/1_Code/0800_Capacity-Reserves/0800_capacity-reserves_phase2.r
+++ 2020phase2/1_Code/0800_Capacity-Reserves/0800_capacity-reserves_phase2.r
@@ -12,7 +12,8 @@
 #-------------------------------------------------------------------
 
 #working directory
-    setwd("O:/Projekte/BevSzen/2020_sandkasten_bad-rok/2020phase2/")
+    library(here)
+    setwd(paste0(here(), "/2020phase2/"))
 
 #general (e.g. packages, colors)
     source("1_Code/0000_General/0000_general_phase2.r")
@@ -30,13 +31,13 @@
 #-------------------------------------------------------------------
     
 #temporary path (since data not on open data yet)    
-    car_path <- "O:/Projekte/BevSzen/2020_sandkasten_bad-rok/2020phase2/2_Data/1_Input/KaReB.csv"
+    car_path <- "2_Data/1_Input/KaReB.csv"
     
-    
+
 #capacity and reserves: data
     car_dat <- read_csv(car_path) %>% 
         rename(year = PublJahr) %>% 
-        gather(car_initial, key = initial, value = area) %>% 
+        pivot_longer(cols = car_initial, names_to = "initial", values_to = "area") %>%      
         left_join(look_car, by = "initial") %>% 
         mutate(distnum = as.numeric(QuarCd)) %>% 
         left_join(look_reg, by = "distnum") %>% 
@@ -166,5 +167,62 @@
 
         lapply(uni_d, p803)
 
-    dev.off()      
+    dev.off()  
+    
+    
+#-------------------------------------------------------------------
+#combine information (with the parameters)
+#-------------------------------------------------------------------
+   
+#current data (and no area values below zero)    
+    curr <- mutate(car_dyepw, area = pmax(0, ha)) %>% 
+        filter(year == max(year)) %>% 
+        select(-c(year, ha))     
+    
+#residence portion
+     residence <- mutate(curr, resi = case_when(residence == uni_e[1] ~ "min", 
+        residence ==  uni_e[2] ~ "real", TRUE ~ "max")) %>% 
+        select(-residence) %>% 
+        pivot_wider(names_from = resi, values_from = area) %>% 
+        mutate(car_resid = car_resi, 
+            area = if_else(car_resid >= 0, real + (max - real) * car_resid/100,
+            real + (real - min) * car_resid/100)) %>% 
+        select(district, plot, owner, cat, area)    
+   
+#plot construction
+    pcon <- mutate(residence, pcon = if_else(plot == uni_p[1], "with", "without")) %>% 
+        select(-plot) %>% 
+        pivot_wider(names_from = pcon, values_from = area) %>% 
+        mutate(area = without + (with - without) * car_plot/100) %>% 
+        select(district, owner, cat, area)    
+    
+#conversion from total to living area
+    living <- mutate(pcon, living = area / ((100 + car_sc)/100)) %>% 
+        select(-area)
+    
+#degree of utilization, proportion of usage
+    usage_prop <- mutate(living, area = if_else(cat %in% car_category[c(1, 4)], 
+        living / car_uti_input * car_uti, living)) %>%
+        select(-living) %>% 
+        pivot_wider(names_from = cat, values_from = area) %>% 
+        #new reserves, usage proportion
+        mutate(reserve_new = capacity - buildings, 
+            usage_prop = pmax(0, pmin(100, 
+            if_else(usage == 0, 0, usage / reserve_new * 100) + car_pp)))
+    
+    
+    
+    
+    
+    
+    
+    
+        
+    
+    
+    
+    car_uti
+      tmp4$flaeche.kap <- tmp4$flaeche.kap / 85 * kareb.ausbau
+    tmp4$flaeche.ina <- tmp4$flaeche.ina / 85 * kareb.ausbau  
+  
     
