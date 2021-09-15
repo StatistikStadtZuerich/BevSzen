68005998523806cd55761e699cde29e525b7e980
Rosin Klemens (SSZ) <sszrok@global.szh.loc>
2021-09-15 14:17:29 +0200

    living area (new code)

--- 2020phase2/1_Code/0900_Living-Space/0900_living-space_phase2.r
+++ 2020phase2/1_Code/0900_Living-Space/0900_living-space_phase2.r
@@ -36,7 +36,7 @@
         left_join(look_dis, by = "QuarCd") %>% 
         mutate(owner = factor(if_else(EigentumCd== 1, uni_w[1], uni_w[2]), uni_w),
             district = factor(distr, uni_d)) %>% 
-        select(year, district, owner, area, people)
+        select(year, district, owner, area, apartments, people)
     
     
 #-------------------------------------------------------------------
@@ -44,7 +44,7 @@
 #-------------------------------------------------------------------
       
 #livings space (y)
-    #WHY? plots to support the base year decision
+    #WHY? e.g. to support the base year decision
     
     spa_y <- group_by(spa_dat, year) %>% 
             summarize(area = sum_NA(area),
@@ -55,39 +55,84 @@
     sszplot(spa_y,
             aes_x = "year", aes_y = "spa_y",
             labs_y = "living space (m² per person)",
+            scale_y = c(0, NA),
             name = "0900_living-space_y",
-            width = 8, height = 6)   
+            width = 8, height = 5)   
 
 
+#-------------------------------------------------------------------
+#living space by year and owner
+#-------------------------------------------------------------------
+      
+#livings space (yo)
     
+    spa_yo <- group_by(spa_dat, year, owner) %>% 
+            summarize(area = sum_NA(area),
+                people = sum_NA(people)) %>% 
+        ungroup() %>% 
+        mutate(spa_yo = round(area / people, round_prop))    
     
-    
+    sszplot(spa_yo,
+            aes_x = "year", aes_y = "spa_yo", aes_col = "owner",
+            labs_y = "living space (m² per person)",
+            scale_y = c(0, NA),
+            name = "0901_living-space_yo",
+            width = 8, height = 5)   
     
 
+#-------------------------------------------------------------------
+#living space (and area, apartments, people) by district, year and owner
+#-------------------------------------------------------------------
+      
+#livings space (dyo)
+    spa_dyo <- group_by(spa_dat, district, year, owner) %>% 
+            summarize(area = sum_NA(area),
+                area_ha = area/10000,
+                apartments = sum_NA(apartments),
+                people = sum_NA(people)) %>% 
+        ungroup() %>% 
+        mutate(spa_dyo = round(area / people, round_prop))    
     
+#plot: living space    
+    sszplot(spa_dyo,
+            aes_x = "year", aes_y = "spa_dyo", aes_col = "owner",
+            labs_y = "living space (m² per person)",            
+            wrap = "district", ncol = 4,
+            scale_y = c(0, NA),            
+            name = "0902_living-space_dyo",
+            width = 12, height = 14)      
     
+#plot: area    
+    sszplot(spa_dyo,
+            aes_x = "year", aes_y = "area_ha", aes_col = "owner",
+            labs_y = "living area (in ha)",            
+            wrap = "district", ncol = 4,
+            scale_y = c(0, NA),            
+            name = "0903_area_dyo",
+            width = 12, height = 14)   
     
-
-    
-
+#plot: apartments    
+    sszplot(spa_dyo,
+            aes_x = "year", aes_y = "apartments", aes_col = "owner",
+            labs_y = "apartments",            
+            wrap = "district", ncol = 4,
+            scale_y = c(0, NA),            
+            name = "0904_apartments_dyo",
+            width = 12, height = 14)   
    
+#plot: people    
+    sszplot(spa_dyo,
+            aes_x = "year", aes_y = "people", aes_col = "owner",
+            labs_y = "people",            
+            wrap = "district", ncol = 4,
+            scale_y = c(0, NA),            
+            name = "0905_people_dyo",
+            width = 12, height = 14)   
+   
+
     
-#     
-# #plot
-#     p900 <- ggplot(data = total) + 
-#         geom_line(aes(x = year, y = usage_prop, color = owner)) + 
-#         facet_wrap(~district, ncol = 4) +      
-#         scale_colour_manual(values = col_w) +  
-#         scale_x_continuous(breaks = pretty_breaks()) + 
-#         labs(x = "", y = "usage proportion (in % of the reserves per year)", color = "") +  
-#         expand_limits(y = 0) +         
-#         neutral +
-#         theme(panel.spacing = unit(1.5, "lines"))      
-#     
-#     ggsave(paste0(spa_res, "0900_living-space_by-district-year.pdf"), 
-#         plot = p900, width = 12, height = 14)     
-#     
-#     
+    
+    
     
     
     
