227699eb8d7fc133cc6e2a30761e7018b3d90668
Klemens Rosin <klemens.rosin@gmx>
2021-09-27 09:59:11 +0200

    new allocation code

---
+++ 2020phase2/1_Code/1000_Allocation/1000_allocation_phase2.r
@@ -0,0 +1,359 @@
+#-------------------------------------------------------------------
+#Allocation
+#
+#
+#
+#rok/bad, October 2021
+#-------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------
+#paths, general
+#-------------------------------------------------------------------
+
+#working directory
+    library(here)
+    setwd(paste0(here(), "/2020phase2/"))
+
+#general (e.g. packages, colors)
+    source("1_Code/0000_General/0000_general_phase2.r")
+
+#result path (for images)
+    aca_res <- "3_Results/1000_Allocation/"
+    
+#export path (for future rates)
+    aca_exp <- "2_Data/4_Rates/" 
+    
+
+
+#-------------------------------------------------------------------
+#import, data preparation
+#-------------------------------------------------------------------
+  
+#allocation: data
+    #WHY living space file? same data as for living space
+    aca_dat <- read_csv(spa_od) %>% 
+        rename(year = StichtagDatJahr, apartments = AnzWhg, people = PersInGeb) %>% 
+        left_join(look_dis, by = "QuarCd") %>% 
+        mutate(owner = factor(if_else(EigentumCd== 1, uni_w[1], uni_w[2]), uni_w),
+            district = factor(distr, uni_d)) %>% 
+        select(year, district, owner, apartments, people)
+    
+    
+#-------------------------------------------------------------------
+#allocation by year
+#-------------------------------------------------------------------
+      
+#allocation (y)
+    #WHY? e.g. to support the base year decision 
+    
+    aca_y <- group_by(aca_dat, year) %>% 
+            summarize(apartments = sum_NA(apartments),
+                people = sum_NA(people)) %>% 
+        ungroup() %>% 
+        mutate(aca_y = round(people / apartments, round_prop))    
+    
+    sszplot(aca_y,
+            aes_x = "year", aes_y = "aca_y",
+            labs_y = "allocation (people per apartment)",
+            scale_y = c(0, NA),
+            name = "1000_allocation_y",
+            width = 8, height = 5)   
+
+
+#-------------------------------------------------------------------
+#allocation by year and owner
+#-------------------------------------------------------------------
+      
+#allocation (yw)
+    
+    aca_yw <- group_by(aca_dat, year, owner) %>% 
+            summarize(apartments = sum_NA(apartments),
+                people = sum_NA(people)) %>% 
+        ungroup() %>% 
+      mutate(aca_yw = round(people / apartments, round_prop))  
+    
+    tail(aca_yw)
+    
+    sszplot(aca_yw,
+            aes_x = "year", aes_y = "aca_yw", aes_col = "owner",
+            labs_y = "allocation (people per apartment)",
+            scale_y = c(0, NA),
+            name = "1001_allocation_yw",
+            width = 8, height = 5)   
+    
+
+#-------------------------------------------------------------------
+#allocation (and apartments, people) by district, year and owner
+#-------------------------------------------------------------------
+      
+#allocation (dyw)
+    aca_dyw <- group_by(aca_dat, district, year, owner) %>% 
+            summarize(
+                apartments = sum_NA(apartments),
+                people = sum_NA(people)) %>% 
+        ungroup() %>% 
+        mutate(aca_dyw = round(people / apartments, round_prop))    
+      
+#look at certain data points    
+    
+    # test_c <- filter(aca_dyw, (year == 2020) & (owner == "cooperative housing") & (apartments < 500)) %>% 
+    #     arrange(apartments)
+    # as.data.frame(test_c)
+    # 
+    # test_p <- filter(aca_dyw, (year == 2020) & (owner == "private housing")) %>%
+    #      arrange(apartments)
+    # 
+    # ew <- filter(aca_dyw, (district == "Escher Wyss") & (owner == "private housing")) %>%
+    #      arrange(year)
+
+    
+    
+#plot: allocation    
+    sszplot(aca_dyw,
+            aes_x = "year", aes_y = "aca_dyw", aes_col = "owner",
+            labs_y = "allocation (people per apartment)",            
+            wrap = "district", ncol = 4,
+            scale_y = c(0, NA),            
+            name = "1002_allocation_dyw",
+            width = 12, height = 14)   
+    
+#plot: apartments    
+    sszplot(aca_dyw,
+            aes_x = "year", aes_y = "apartments", aes_col = "owner",
+            labs_y = "apartments",            
+            wrap = "district", ncol = 4,
+            scale_y = c(0, NA),            
+            name = "1003_apartments_dyw",
+            width = 12, height = 14)   
+   
+#plot: people    
+    sszplot(aca_dyw,
+            aes_x = "year", aes_y = "people", aes_col = "owner",
+            labs_y = "people",            
+            wrap = "district", ncol = 4,
+            scale_y = c(0, NA),            
+            name = "1004_people_dyw",
+            width = 12, height = 14)   
+   
+    
+
+#-------------------------------------------------------------------
+#prediction: entire city, by owner
+#-------------------------------------------------------------------
+        
+#base years 
+    spa_yw_base <- filter(spa_yw, 
+            (year >= spa_base_begin) & (year <= spa_base_end)) %>% 
+        select(year, owner, spa_yw)
+    
+#prediction (no living space below zero)   
+    spa_yw_pred <- con_reg(data = spa_yw_base, x = "year", y = "spa_yw",
+              group_cols = "owner",
+              window = spa_window_thres, base_t0 = spa_base_begin,
+              szen_t0 = szen_begin, szen_t1 = szen_end,
+              prop_trend = spa_prop_trend, thres_percent = spa_thres_percent,
+              lower_thres = 0)   
+    
+#past and prediction
+    spa_yw_past_pred <- as_tibble(expand_grid(
+            year = (min(spa_yw$year)):szen_end,
+            owner = uni_w)) %>% 
+        left_join(select(spa_yw, year, owner, spa_yw), by = c("year", "owner")) %>%     
+        left_join(select(spa_yw_pred, year, owner, pred_roll),
+            by = c("year", "owner")) %>% 
+        mutate(spa_yw_all = if_else(year < szen_begin, spa_yw, pred_roll))    
+    
+#plot
+    sszplot(spa_yw_past_pred,
+            aes_x = "year", aes_y = "spa_yw_all", aes_col = "owner",
+            labs_y = "living space (m² per person)",
+            i_x = c(spa_base_begin, spa_base_end), 
+            scale_y = c(0, NA),               
+            name = "0906_living-space_prediction_yw",
+            width = 10, height = 7)      
+    
+    
+#-------------------------------------------------------------------
+#prediction: by district and owner
+#-------------------------------------------------------------------
+        
+#base years 
+    spa_dyw_base <- filter(spa_dyw, 
+            (year >= spa_base_begin) & (year <= spa_base_end)) %>% 
+        select(district, year, owner, spa_dyw)    
+    
+#prediction (no living space below zero)   
+    spa_dyw_pred <- con_reg(data = spa_dyw_base, x = "year", y = "spa_dyw",
+              group_cols = c("district", "owner"),
+              window = spa_window_thres, base_t0 = spa_base_begin,
+              szen_t0 = szen_begin, szen_t1 = szen_end,
+              prop_trend = spa_prop_trend, thres_percent = spa_thres_percent,
+              lower_thres = 0)   
+    
+#past and prediction
+    spa_dyw_past_pred <- as_tibble(expand_grid(
+            district = uni_d,
+            year = (min(spa_dyw$year)):szen_end,
+            owner = uni_w)) %>% 
+        left_join(spa_dyw, by = c("district", "year", "owner")) %>%     
+        left_join(select(spa_dyw_pred, district, year, owner, pred_roll),
+            by = c("district", "year", "owner")) %>% 
+        mutate(spa_dyw_all = if_else(year < szen_begin, spa_dyw, pred_roll))      
+    
+#plot   
+    sszplot(spa_dyw_past_pred,
+            aes_x = "year", aes_y = "spa_dyw_all", aes_col = "owner",
+            labs_y = "living space (m² per person)",            
+            wrap = "district", ncol = 4,
+            i_x = c(spa_base_begin, spa_base_end),           
+            scale_y = c(0, NA),            
+            name = "0907_living-space_precition_dyw",
+            width = 12, height = 14)     
+    
+    
+    
+#-------------------------------------------------------------------
+#prediction: Escher Wyss, by owner
+#-------------------------------------------------------------------
+   
+        
+#WHY a different approach for the Escher Wyss district, private housing?
+    # the living space over time differs substantially from the other districts
+    # (increase until 2013, then decrease)
+    # reason: few apartments in 2008 then very intense construction activity
+    # numbers: less than 1500 apartments in 2008
+    # more than 3100 apartments in 2017
+    # after construction some apartments were finished, yet no one lived there yet
+    # for a certain moment the empty apartments increase the living space (based on buildings)
+    # due to the high ratio of new buildings (compared to existing buildings) the living space was increased
+    # this will not happen again in future (due to the higher amount of buildings)
+    
+#WHY with owner?
+    #con_reg function needs groups (so far)
+     
+#Escher Wyss (district number: 52)
+    spa_dyw_52 <- filter(spa_dyw, district == "Escher Wyss") %>% 
+        select(year, owner, spa_dyw)
+    
+#base years 
+    spa_dyw_base_52 <- filter(spa_dyw_52, 
+            (year >= spa_base_begin_52p) & (year <= spa_base_end))
+    
+#prediction (no living space below zero)   
+    spa_dyw_pred_52 <- con_reg(data = spa_dyw_base_52, x = "year", y = "spa_dyw",
+              group_cols = "owner",  
+              window = spa_window_thres, base_t0 = spa_base_begin_52p,
+              szen_t0 = szen_begin, szen_t1 = szen_end,
+              prop_trend = spa_prop_trend_52p, thres_percent = spa_thres_percent,
+              lower_thres = 0) 
+    
+#past and prediction
+    spa_yw_past_pred_52 <- as_tibble(expand_grid(
+            year = (min(spa_yw$year)):szen_end,
+            owner = uni_w)) %>% 
+        left_join(select(spa_dyw_52, year, owner, spa_dyw), by = c("year", "owner")) %>%     
+        left_join(select(spa_dyw_pred_52, year, owner, pred_roll),
+            by = c("year", "owner")) %>% 
+        mutate(spa_dyw_all = if_else(year < szen_begin, spa_dyw, pred_roll), 
+            district = uni_d[uni_d == "Escher Wyss"])
+    
+#plot
+    sszplot(spa_yw_past_pred_52,
+            aes_x = "year", aes_y = "spa_dyw_all", aes_col = "owner",
+            labs_y = "living space (m² per person)",
+            i_x = c(spa_base_begin_52p, spa_base_end), 
+            scale_y = c(0, NA),               
+            name = "0908_living-space_prediction_52yw",
+            width = 10, height = 7)  
+    
+    
+#-------------------------------------------------------------------
+#combine the different data sets
+#-------------------------------------------------------------------
+
+#district, year, owner
+    spa_prep_dyw <- select(spa_dyw_past_pred, district, year, owner, spa_dyw_all) %>%  
+        rename(spa_dyw = spa_dyw_all)
+
+#year, owner
+    spa_prep_yw <- select(spa_yw_past_pred, year, owner, spa_yw_all) %>%  
+        rename(spa_yw = spa_yw_all)
+    
+#Escher Wyss (only future: values of the past already contained in 'spa_prep_dyo')
+    spa_prep_52p <- select(spa_yw_past_pred_52, district, year, owner, spa_dyw_all) %>%  
+        filter((owner == uni_w[2]) & (year >= szen_begin)) %>% 
+        rename(spa_52p = spa_dyw_all)
+    
+#apartment threshold (mean over base years)
+    
+    #WHY with expand_grid?
+    #correct calculation if no apartments (by owner) in a certain district and year
+    
+    spa_apart_thres <- as_tibble(expand_grid(
+            district = uni_d,
+            year = spa_base_begin:spa_base_end,
+            owner = uni_w)) %>% 
+        left_join(select(spa_dyw, district, year, owner, apartments), 
+            by = c("district", "year", "owner")) %>% 
+        replace_na(list(apartments = 0)) %>% 
+        group_by(district, owner) %>% 
+            summarize(apart_thres = mean(apartments)) %>% 
+        ungroup()
+        
+#combine the data sets
+    spa_comb <- spa_prep_dyw %>% 
+        left_join(spa_prep_yw, by = c("year", "owner")) %>% 
+        left_join(spa_prep_52p, by = c("district", "year", "owner")) %>% 
+        left_join(spa_apart_thres, by = c("district", "owner")) %>% 
+        mutate(spa = if_else(year < szen_begin, spa_dyw, 
+            if_else((district == "Escher Wyss") & (owner == uni_w[2]), spa_52p, 
+                if_else(apart_thres < spa_apart, spa_yw, spa_dyw))))
+   
+#plot: past and prediction
+    sszplot(spa_comb,
+            aes_x = "year", aes_y = "spa", aes_col = "owner",
+            labs_y = "living space (m² per person)",            
+            wrap = "district", ncol = 4,
+            i_x = c(spa_base_begin, spa_base_end),           
+            scale_y = c(0, NA),            
+            name = "0909_living-space_precition_dyw_different-datasets",
+            width = 12, height = 14) 
+    
+#plot both (i.e. per district, and entire city)
+    
+    names_before <- c("spa_dyw", "spa_yw", "spa")
+    names_plot <- c("by district", "entire city", "prediction")
+    
+    spa_both <- select(spa_comb, district, year, owner, spa_dyw, spa_yw, spa) %>% 
+        pivot_longer(cols = names_before, names_to = "category", values_to = "spa") %>% 
+        mutate(cat = factor(case_when(category ==  names_before[1] ~ names_plot[1], 
+                                            category  ==  names_before[2] ~ names_plot[2],
+                                            TRUE ~ names_plot[3]), levels = names_plot))
+    
+#same content, different plot
+    sszplot(spa_both,
+            aes_x = "year", aes_y = "spa", aes_col = "cat", 
+            grid = c(".", "owner"),
+            labs_y = "living space (m² per person)",
+            i_x = c(spa_base_begin, spa_base_end),           
+            scale_y = c(0, NA),             
+            name = "0910_living-space_dyw-yw",
+            width = 8, height = 4,
+            multi = uni_d)     
+    
+    
+#-------------------------------------------------------------------
+#export the results
+#-------------------------------------------------------------------
+
+#export data   
+    spa_ex_data <- mutate(spa_comb, spa_dyw = round(spa, round_area)) %>% 
+        select(district, year, owner, spa_dyw) %>%
+        filter(year >= szen_begin) %>% 
+        arrange(district, year, owner)
+      
+#export
+    write_csv(spa_ex_data, paste0(spa_exp, "living-space_future.csv"))    
+
+    
