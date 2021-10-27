6d7ea21ddc0f82e5c2b91a115dc219385dd93fb7
Klemens Rosin <klemens.rosin@gmx>
2021-10-27 08:33:59 +0200

    ugly style (back-correction)

--- 2020phase2/1_Code/1000_Allocation/1000_allocation_phase2.r
+++ 2020phase2/1_Code/1000_Allocation/1000_allocation_phase2.r
@@ -10,19 +10,18 @@
 #-------------------------------------------------------------------
 #paths, general
 #-------------------------------------------------------------------
-if (!exists("para")) {
-#working directory
-    library(here)
-    setwd(paste0(here(), "/2020phase2/"))
 
-#general (e.g. packages, colors)
-    source("1_Code/0000_General/0000_general_phase2.r")
-}
-
-#export path (for future rates)
-    aca_exp <- exp_path 
-    
-
+#general functions already available?
+    if (!exists("para")) {
+      
+        #working directory
+            library(here)
+            setwd(paste0(here(), "/2020phase2/"))
+        
+        #general (e.g. packages, colors)
+            source("1_Code/0000_General/0000_general_phase2.r")
+            
+    }
 
 #-------------------------------------------------------------------
 #import, data preparation
