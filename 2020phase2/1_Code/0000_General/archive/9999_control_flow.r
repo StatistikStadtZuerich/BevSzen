94a1bb362a555e093aca40685f988c1cefc8a845
sszbad <daniel.baumgartner.ssz@zuerich.ch>
2021-12-22 18:31:42 +0100

    adapted old control flow file since new one is missing

--- 2020phase2/1_Code/0000_General/archive/9999_control_flow.r
+++ 2020phase2/1_Code/0000_General/archive/9999_control_flow.r
@@ -18,14 +18,31 @@
 
 ## TODO: some log
 
-run_pops <- function(modules = c("all")) {
+i_scen = "middle"
+run_pops <- function(modules = c("all"), i_scen = "middle") {
   setwd(paste0(here::here(), "/2020phase2"))
   code <- "1_Code/"
-  term <- "_phase2.r"
-
-  source(paste0(code, "0000_General/0000_general", term))
+  term <- ".r"
+  
+  #general functions already available?
+  if (!exists("para")) {
+    #general functions (without dependence on parameters)
+    source("1_Code/0000_General/0000_general_without-parameters.r")
     
-  if (modules %in% c("all", "birth")) {
+    #parameters (depend on scenario)
+    for(i_para in 1:nrow(para)){
+      assign(para$parameter[i_para], para[[i_scen]][i_para],
+             envir = .GlobalEnv)}
+    
+    #general functions (with dependence on parameters)
+    source(paste0(code_path, "/0000_General/0001_general_with-parameters.r"))
+    
+  }
+  
+
+  # source(paste0(code, "0000_General/0000_general", term))
+    
+  if (modules %in% c("all", "demo", "birth")) {
     source(paste0(code, "0100_Birth/0100_birth-fertility", term))
     source(paste0(code, "0100_Birth/0110_birth-origin", term))
     source(paste0(code, "0100_Birth/0120_birth-sex-ratio", term))
@@ -49,14 +66,23 @@ run_pops <- function(modules = c("all")) {
   if (modules %in% c("all", "demo", "naturalization"))
     source(paste0(code, "0700_Naturalization/0700_naturalization", term))
   
-  if (modules %in% c("all", "live", "capacity")) {}
+  if (modules %in% c("all", "live", "capacity"))
     source(paste0(code, "0800_Capacity-Reserves/0800_capacity-reserves", term))
     
-  if (modules %in% c("all", "live", "living"))  {}
-    # source(paste0(code, "0900_Living-Space/0900_Living-Space", term))
+  if (modules %in% c("all", "live", "living"))
+    source(paste0(code, "0900_Living-Space/0900_living-space", term))
 
-  if (modules %in% c("all", "live", "alloc"))  {}
-    # source(paste0(code, "1000_Allocation/1000_allocation", term))
+  if (modules %in% c("all", "live", "alloc"))
+    source(paste0(code, "1000_Allocation/1000_allocation", term))
+  
+  if (modules %in% c("all"))
+    source(paste0(code, "1100_Projects/1100_projects", term))
+  
+  if (modules %in% c("all"))
+    source(paste0(code, "1200_Ownership/1200_ownership", term))
+  
+  if (modules %in% c("all"))
+    source(paste0(code, "1300_Housing-Model/1300_housing-model", term))
 }
 
 system.time(run_pops())
