058706052087690ca6f90acc6479ddb6bfd9552a
Rosin Klemens (SSZ) <sszrok@global.szh.loc>
2022-04-05 09:15:21 +0200

    style

--- 2020phase2/1_Code/0100_Birth/0120_birth-sex-ratio.r
+++ 2020phase2/1_Code/0100_Birth/0120_birth-sex-ratio.r
@@ -1,108 +1,117 @@
 #-------------------------------------------------------------------
-#Birth: Sex Ratio
+# birth: sex ratio
 #
 #
 #
-#rok/bad
+# rok/bad
 #-------------------------------------------------------------------
 
 
-#-------------------------------------------------------------------
-#paths, general
-#-------------------------------------------------------------------
-
-#general functions already available?
-    if (!exists("para")) {
-      
-        #working directory
-            library(here)
-            setwd(paste0(here(), "/2020phase2/"))
-        
-        #general functions (without dependence on parameters)
-            source("1_Code/0000_General/0002_general_without-parameters.r")
-            
-        #parameters (depend on scenario)
-            i_scen <- "middle"
-            for(i_para in 1:nrow(para)){
-                assign(para$parameter[i_para], para[[i_scen]][i_para],
-                       envir = .GlobalEnv)}
-
-        #general functions (with dependence on parameters)
-            source(paste0(code_path, "/0000_General/0003_general_with-parameters.r"))
-    
-    }
-
-#start time
-    t0 <- Sys.time() 
-
-    
-#-------------------------------------------------------------------
-#import, proportion male
-#-------------------------------------------------------------------
-
-#proportion male
-    pro_male <- read_csv(bir_od) %>% 
-        rename(year = EreignisDatJahr, bir = AnzGebuWir) %>% 
-        mutate(sex = factor(if_else(SexCd == 1, uni_s[1], uni_s[2]), uni_s)) %>% 
-        select(year, sex, bir) %>%     
-        group_by(year, sex) %>% 
-            summarize(bir = sum(bir),
-                      .groups = "drop") %>% 
-        spread(key = sex, value = bir) %>% 
-        mutate(pro_male = round(male / (male + female) * 100, round_rate))
-    
-#plot   
-    year5 <- pro_male$year[pro_male$year %% 5 == 0]
-
-    sszplot(pro_male,
-            aes_x = "year", aes_y = "pro_male",
-            geom = c("line", "point"),
-            i_x = year5, i_y = 50,
-            labs_y = "proportion male in %",
-            scale_y = c(0, 70), breaks = seq(0, 70, 10),
-            name = "0190_sex-ratio_by-year")  
-    
-#-------------------------------------------------------------------
-#prediction
-#-------------------------------------------------------------------
-   
-#model
-    #the sex proportion could depend on age 
-    #see: https://www.pnas.org/content/112/16/E2102, Figure 4
-    #however, a simple model is used: mean only
-    
-#mean
-    pred_mean <- pro_male %>% 
-        filter(year >= bir_sex_ratio_begin & year <= bir_sex_ratio_end) %>%
-        summarize(pred_mean = mean(pro_male),
-                  .groups = "drop")
-    
-#plot with mean
-    sszplot(pro_male,
-            aes_x = "year", aes_y = "pro_male",
-            geom = c("line", "point"),
-            i_x = year5, i_y = pred_mean$pred_mean,
-            labs_y = "proportion male in %",
-            scale_y = c(0, 70), breaks = seq(0, 70, 10),
-            name = "0191_sex-ratio_by-year_with-mean")
-
-    
-    
-#nice option: prediction dashed blue (instead of grey; in contrast to 50% value in previous plot)    
 
 #-------------------------------------------------------------------
-#export the results
+# paths, general
 #-------------------------------------------------------------------
 
-#proportion male: prediction   
-    pro_male_pred <- tibble(year = szen_begin:szen_end, 
-        pro_male = round(pred_mean$pred_mean, round_rate))     
-        
-#export
-    write_csv(pro_male_pred, paste0(exp_path, "/birth_sex-ratio_future.csv"))    
+# general functions already available?
+if (!exists("para")) {
 
-#log info    
-    cat_log(paste0("proportion male babies: ", 
-        capture.output(Sys.time() - t0)))
-    
-    
+  # working directory
+  library(here)
+  setwd(paste0(here(), "/2020phase2/"))
+
+  # general functions (without dependence on parameters)
+  source("1_Code/0000_General/0002_general_without-parameters.r")
+
+  # parameters (depend on scenario)
+  i_scen <- "middle"
+  for (i_para in 1:nrow(para)) {
+    assign(para$parameter[i_para], para[[i_scen]][i_para],
+      envir = .GlobalEnv
+    )
+  }
+
+  # general functions (with dependence on parameters)
+  source(paste0(code_path, "/0000_General/0003_general_with-parameters.r"))
+}
+
+# start time
+t0 <- Sys.time()
+
+
+#-------------------------------------------------------------------
+# import, proportion male
+#-------------------------------------------------------------------
+
+# proportion male
+pro_male <- read_csv(bir_od) %>%
+  rename(year = EreignisDatJahr, bir = AnzGebuWir) %>%
+  mutate(sex = factor(if_else(SexCd == 1, uni_s[1], uni_s[2]), uni_s)) %>%
+  select(year, sex, bir) %>%
+  group_by(year, sex) %>%
+  summarize(
+    bir = sum(bir),
+    .groups = "drop"
+  ) %>%
+  spread(key = sex, value = bir) %>%
+  mutate(pro_male = round(male / (male + female) * 100, round_rate))
+
+# plot
+year5 <- pro_male$year[pro_male$year %% 5 == 0]
+
+sszplot(pro_male,
+  aes_x = "year", aes_y = "pro_male",
+  geom = c("line", "point"),
+  i_x = year5, i_y = 50,
+  labs_y = "proportion male in %",
+  scale_y = c(0, 70), breaks = seq(0, 70, 10),
+  name = "0190_sex-ratio_by-year"
+)
+
+#-------------------------------------------------------------------
+# prediction
+#-------------------------------------------------------------------
+
+# model
+# the sex proportion could depend on age
+# see: https://www.pnas.org/content/112/16/E2102, Figure 4
+# however, a simple model is used: mean only
+
+# mean
+pred_mean <- pro_male %>%
+  filter(year >= bir_sex_ratio_begin & year <= bir_sex_ratio_end) %>%
+  summarize(
+    pred_mean = mean(pro_male),
+    .groups = "drop"
+  )
+
+# plot with mean
+sszplot(pro_male,
+  aes_x = "year", aes_y = "pro_male",
+  geom = c("line", "point"),
+  i_x = year5, i_y = pred_mean$pred_mean,
+  labs_y = "proportion male in %",
+  scale_y = c(0, 70), breaks = seq(0, 70, 10),
+  name = "0191_sex-ratio_by-year_with-mean"
+)
+
+
+#should be changed: prediction dashed blue (instead of grey; in contrast to 50% value in previous plot)
+
+#-------------------------------------------------------------------
+# export the results
+#-------------------------------------------------------------------
+
+# proportion male: prediction
+pro_male_pred <- tibble(
+  year = szen_begin:szen_end,
+  pro_male = round(pred_mean$pred_mean, round_rate)
+)
+
+# export
+write_csv(pro_male_pred, paste0(exp_path, "/birth_sex-ratio_future.csv"))
+
+# log info
+cat_log(paste0(
+  "proportion male babies: ",
+  capture.output(Sys.time() - t0)
+))
