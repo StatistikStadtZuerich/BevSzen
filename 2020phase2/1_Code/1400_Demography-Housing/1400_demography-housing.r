5c20dbb8def333ed84ef55520b96a6ab9a3bbaa4
Rosin Klemens (SSZ) <Klemens.Rosin@zuerich.ch>
2022-05-24 15:59:49 +0200

    work flow

--- 2020phase2/1_Code/1400_Demography-Housing/1400_demography-housing.r
+++ 2020phase2/1_Code/1400_Demography-Housing/1400_demography-housing.r
@@ -88,7 +88,7 @@ mor <- read_csv(paste0(exp_path, "/mortality_future.csv"), lazy = FALSE) %>%
 # immigration*: immigration* rate
 ims_rate <- read_csv(paste0(exp_path, "/immigration-star_rate-dy_future.csv"), lazy = FALSE) %>%
   mutate(district = factor(district, levels = uni_d)) %>% 
-  mutate(rate = if_else(district == "Wollishofen", 5, rate))
+  mutate(rate = if_else(district == "Wollishofen", 1, rate))
 
 # immigration*: sex and origin proportion
 ims_prop_so <- read_csv(paste0(exp_path, "/immigration-star_prop-so-dy_future.csv"), lazy = FALSE) %>%
