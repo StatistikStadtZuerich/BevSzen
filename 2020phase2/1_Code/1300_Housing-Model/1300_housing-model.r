01626bea42d8b818feb43dfa2dbe87e7755d179c
Rosin Klemens (SSZ) <Klemens.Rosin@zuerich.ch>
2022-05-28 14:09:30 +0200

    test

--- 2020phase2/1_Code/1300_Housing-Model/1300_housing-model.r
+++ 2020phase2/1_Code/1300_Housing-Model/1300_housing-model.r
@@ -266,29 +266,29 @@ project_reserves <- function(x, ...) {
       x$pop[i] <- x$project[i] - (x$project[i] - x$car[i]) * (car_trust/100)
     }
 
-    # correction of the reserves (if too much used due to the project list)
-    x$corr[i] <- max(0, x$pop[i] - x$car[i])
-
-    # correction (proportional to the reserve usage)
-     
-    if (i < nrow(x)) {
-      index <- (i + 1):nrow(x)
-      x$subtract <- 0
-
-      # correction proportional to usage
-      if (sum(x$usage[index]) > 0) {
-        x$subtract[index] <- x$usage[index] / sum(x$usage[index]) * x$corr[i]
-      } else {
-        x$subtract[index] <- 0
-      }
-
-      # subtract (usage per year, and cumulative usage plus population)
-      x$usage[index] <- pmax(0, x$usage[index] - x$subtract[index])
-      
-      # new reserves value based on corrected usage
-      x$car[i+1] <- pmax(0, x$pop[i] + x$usage[i+1])
-      
-    }
+    # # correction of the reserves (if too much used due to the project list)
+    # x$corr[i] <- max(0, x$pop[i] - x$car[i])
+    # 
+    # # correction (proportional to the reserve usage)
+    #  
+    # if (i < nrow(x)) {
+    #   index <- (i + 1):nrow(x)
+    #   x$subtract <- 0
+    # 
+    #   # correction proportional to usage
+    #   if (sum(x$usage[index]) > 0) {
+    #     x$subtract[index] <- x$usage[index] / sum(x$usage[index]) * x$corr[i]
+    #   } else {
+    #     x$subtract[index] <- 0
+    #   }
+    # 
+    #   # subtract (usage per year, and cumulative usage plus population)
+    #   x$usage[index] <- pmax(0, x$usage[index] - x$subtract[index])
+    #   
+    #   # new reserves value based on corrected usage
+    #   x$car[i+1] <- pmax(0, x$pop[i] + x$usage[i+1])
+    #   
+    # }
 
     # end of the loop over years
   }
