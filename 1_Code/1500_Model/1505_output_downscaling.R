6854d48da4ca4e9ee596ddd985d2cb7a07a2c9e6
insilentio <insilentio@protonmail.ch>
2022-11-11 17:07:55 +0100

    added flaeche calculations styler changes

--- 1_Code/1500_Model/1505_output_downscaling.R
+++ 1_Code/1500_Model/1505_output_downscaling.R
@@ -2,7 +2,7 @@
 # header ------------------------------------------------------------------
 
 # calculate values for downscaling request by AfS
-# 
+#
 # for the years 2021, 2037, 2040, 2045, 2050
 
 # prep work ---------------------------------------------------------------
@@ -13,7 +13,7 @@ source("1_Code/0000_General/0002_general_without-parameters.r")
 i_scen <- "middle"
 for (i_para in 1:nrow(para)) {
   assign(para$parameter[i_para], para[[i_scen]][i_para],
-         envir = .GlobalEnv
+    envir = .GlobalEnv
   )
 }
 # general functions (with dependence on parameters)
@@ -32,9 +32,8 @@ if (!dir.exists(req_path)) {
 req_path <- paste0(req_path, "pop_scen_downscaling.csv")
 
 
+# check some things -------------------------------------------------------
 
-# load data ---------------------------------------------------------------
-# library(readr)
 read_csv("2_Data/4_Rates/middle/living-space_future.csv")
 spa_dyw
 spa_dyw_past_pred
@@ -51,39 +50,48 @@ car_dat
 # same as:
 # usage_area <- read_csv("2_Data/4_Rates/middle/usage_area.csv")
 
+# check parameters used for flaeche.ina (should be 50 and 85 according to spec)
+car_plot
+car_uti
+
+# load data ---------------------------------------------------------------
 kareb <- read_csv("2_Data/1_Input/KaReB.csv") %>%
   group_by(QuarCd, PublJahr, ArealCd, WohnanteilCd, EigentumGrundstkCd) %>%
-  summarise(Inanspruchnahme = sum(Inanspruchnahme))
-kareb %>% filter(QuarCd == "010") %>% print(n=Inf)
-
+  summarise(Inanspruchnahme = sum(Inanspruchnahme)) %>%
+  filter(PublJahr == scen_begin, ArealCd == 1, WohnanteilCd == 2) %>%
+  ungroup() %>%
+  mutate(EigentumGrundstkCd = as.character(EigentumGrundstkCd))
 
 
 
 # prepare output data based on spa_dyw
 # this is still according to the format of last year, transposing to long comes later
-downscale <- spa_dyw_past_pred %>%
-  filter(year >= scen_begin-1) %>%
-  left_join(lookup_map, by = "district") %>% 
+downscale <-
+  spa_dyw_past_pred %>%
+  filter(year >= scen_begin - 1) %>%
+  left_join(lookup_map, by = "district") %>%
   left_join(tibble(ownerCd = labels(uni_w), owner = uni_w), by = "owner") %>%
   mutate(wohnflaeche.ksj = NA, flaeche.ina = NA, flaeche.ina.eff = NA, ratio.ina.eff = NA, bq.ksj = NA) %>%
-  rename(wohnflaeche.lbj = area, wf.ksj = spa_dyw_all, anz.wohn.ksj = apartments,	anz.pers.ksj = people) %>%
-  select(year, QuarCd, district, ownerCd, owner, wohnflaeche.lbj,	wohnflaeche.ksj, flaeche.ina, flaeche.ina.eff, ratio.ina.eff, bq.ksj, wf.ksj, anz.wohn.ksj, anz.pers.ksj)
-
-
-downscale %>%
+  rename(wohnflaeche.lbj = area, wf.ksj = spa_dyw_all, anz.wohn.ksj = apartments, anz.pers.ksj = people) %>%
+  select(
+    year, QuarCd, district, ownerCd, owner, wohnflaeche.lbj, wohnflaeche.ksj, flaeche.ina, flaeche.ina.eff,
+    ratio.ina.eff, bq.ksj, wf.ksj, anz.wohn.ksj, anz.pers.ksj
+  ) %>%
   # add the population
   left_join(pop_total, by = c("district", "year", "owner")) %>%
   mutate(anz.pers.ksj = rowSums(tibble(.$anz.pers.ksj, .$pop), na.rm = TRUE)) %>%
   mutate(wohnflaeche.ksj = wf.ksj * anz.pers.ksj) %>%
-  select( -pop) %>%
-  # calculate delta population and derive flaeche.ina.eff
-  left_join((spa_dyw %>% filter(year == max(year)) %>% select(district, owner, people)), by =  c("district", "owner")) %>%
-  mutate(delta_pop = anz.pers.ksj - people) %>%
-  mutate(flaeche.ina.eff = delta_pop * wf.ksj) %>%
+  select(-pop) %>%
   # add bq.ksj
   left_join(aca_comb, by = c("district", "year", "owner")) %>%
   mutate(bq.ksj = aca) %>%
   select(-aca_dyw, -aca_yw, -aca_52p, -aca, -apart_thres) %>%
-  mutate(anz.wohn.ksj = anz.pers.ksj / bq.ksj)
-
-  
+  mutate(anz.wohn.ksj = anz.pers.ksj / bq.ksj) %>%
+  # calculate delta population and derive flaeche.ina.eff
+  left_join((spa_dyw %>% filter(year == max(year)) %>% select(district, owner, people)), by = c("district", "owner")) %>%
+  mutate(delta_pop = anz.pers.ksj - people) %>%
+  mutate(flaeche.ina.eff = delta_pop * wf.ksj) %>%
+  # add flaeche.ina from kareb and calculate ratio
+  left_join(kareb %>% select(QuarCd, ownerCd = EigentumGrundstkCd, Inanspruchnahme), by = c("QuarCd", "ownerCd")) %>%
+  mutate(flaeche.ina = Inanspruchnahme) %>%
+  mutate(raatio.ina.eff = flaeche.ina.eff / flaeche.ina)
