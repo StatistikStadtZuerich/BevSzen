cb27d741514e9d37d879eec071296729a8e716c0
sszbad <daniel.baumgartner.ssz@zuerich.ch>
2023-01-13 15:37:28 +0100

    added comments, one more check

--- 1_Code/test.R
+++ 1_Code/test.R
@@ -1,7 +1,14 @@
+## test for equalness of two tibbles
+## idea: compare csv's from origiinal code run vs. current scenario run
+## to make sure any changes in code do not affect output
+## Files in consideration are in 2_Data/4_Rates and 2_Data/5_Outputs
+## Prerequisite: original outputs to be stored in data_freeze/2_Data
 
+
+# load necessary variables first
 util_gf()
 
-
+# prepare list of csv's to be compared
 list_current_rates <- list.files(paste0(data_path, "4_Rates"), recursive = TRUE, full.names = TRUE)
 list_freeze_rates <- list.files(paste0("data_freeze/", data_path, "4_Rates"), recursive = TRUE, full.names = TRUE)
 list_current_outputs <- list.files(paste0(data_path, "5_Outputs"), recursive = TRUE, full.names = TRUE)
@@ -10,8 +17,14 @@ list_freeze_outputs <- list.files(paste0("data_freeze/", data_path, "5_Outputs")
 
 
 
-
+# function accepts two character vectors with paths to csv files
+# and compares them for equality
 check_eq <- function(current, freeze) {
+  
+  if (length(current) != length(freeze)) {
+    out <- "file lists do not have same length"
+    return(out)
+  }
   
   out <- character(length(current))
   
@@ -24,11 +37,10 @@ check_eq <- function(current, freeze) {
     if (!isTRUE(check)) {
       out[i] <- paste("files not equal:",
                       "first file:", current[i],
-                      "second file:", freeze[i])
+                      ";  second file:", freeze[i])
       break
     } else {
-      out[i] <- paste("files equal:",
-                      "first file:", current[i])
+      out[i] <- paste("files equal:", current[i])
     }
   }
   
@@ -36,12 +48,8 @@ check_eq <- function(current, freeze) {
 }
 
 
+# compare rates
+eq_rates <- check_eq(list_current_rates, list_freeze_rates)
 
-check_eq(list_current_rates, list_freeze_rates)
-
-check_eq(list_current_outputs, list_freeze_outputs)
-
-
-
-current <- list_current_outputs
-freeze <- list_freeze_outputs
+# compare outputs
+eq_outputs <- check_eq(list_current_outputs, list_freeze_outputs)
