8c8f43fdc03859cb6242f878e1a518402101b9d0
sszbad <daniel.baumgartner.ssz@zuerich.ch>
2023-04-19 20:43:41 +0200

    new way of running the whole calc

--- 1_Code/0000_General/0001_model_control-flow.r
+++ 1_Code/0000_General/0001_model_control-flow.r
@@ -2,15 +2,15 @@ source("1_code/0000_general/general_utils.R")
 
 time_start <- Sys.time()
 
-run_scen(
-    scenarios = c("lower", "middle", "upper"),
-    modules = c("all"))
+# run_scen(
+#     scenarios = c("lower", "middle", "upper"),
+#     modules = c("all"))
 
 # run_scen(
 #     scenarios = c("lower", "middle", "upper"),
 #     modules = c("out"))
 
+util_gf()
+render_books()
+
 Sys.time() - time_start
-
-
-
