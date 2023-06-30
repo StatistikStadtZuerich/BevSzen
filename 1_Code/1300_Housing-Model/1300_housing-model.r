0377a4c91c6db8c7ec4050837bcc78761a536319
Daniel Baumgartner <daniel.baumgartner.ssz@zuerich.ch>
2023-06-30 17:23:53 +0200

    fix column name

--- 1_Code/1300_Housing-Model/1300_housing-model.r
+++ 1_Code/1300_Housing-Model/1300_housing-model.r
@@ -508,7 +508,7 @@ pop_all <- pop_fut_past %>%
 # export the results ------------------------------------------------------
 
 # per district and ownership
-write_csv(pop_fut_past %>% arrange(district, year, ownership),
+write_csv(pop_fut_past %>% arrange(district, year, owner),
           paste0(exp_path, "/housing_model_population_dw"))
 
 # per district
