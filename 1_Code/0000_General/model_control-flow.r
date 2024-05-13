6d2a02f27dbf7158390d8a9b936c9a5875ad951d
sszrok <klemens.rosin@zuerich.ch>
2024-05-13 17:15:30 +0200

    model control

--- 1_Code/0000_General/model_control-flow.r
+++ 1_Code/0000_General/model_control-flow.r
@@ -3,9 +3,11 @@ params <- init("middle_birth_upper")
 
 time_start <- Sys.time()
 
-run_scen(
-    scenarios = c("lower", "middle", "middle_birth_lower", "middle_birth_upper", "upper"),
-    modules = c("ims"))
+# run_scen(
+#     scenarios = c("lower", "middle", "middle_birth_lower", "middle_birth_upper", "upper"),
+#     modules = c("out"))
+
+Sys.time() - time_start
 
 # run_scen(
 #     scenarios = c("lower", "middle", "middle_birth_lower", "middle_birth_upper", "upper"),
