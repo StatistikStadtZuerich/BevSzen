3a3922e7c3b4d5bb7ddbdc4b6a3df8cc2059e181
sszrok <klemens.rosin@zuerich.ch>
2024-05-15 12:36:22 +0200

    run modules

--- 1_Code/0000_General/model_control-flow.r
+++ 1_Code/0000_General/model_control-flow.r
@@ -1,11 +1,11 @@
 source("1_code/0000_general/general_init.R")
-params <- init("middle_birth_upper")
+params <- init("middle_birth_lower")
 
 time_start <- Sys.time()
 
 run_scen(
-    scenarios = c("middle", "middle_birth_lower", "middle_birth_upper"),
-    modules = c("alw"))
+    scenarios = c("middle", "middle_birth_lower"),
+    modules = c("bir"))
 
 Sys.time() - time_start
 
@@ -14,3 +14,6 @@ Sys.time() - time_start
 #     modules = c("all"))
 
 render_book(cache_refresh = TRUE)
+
+
+
