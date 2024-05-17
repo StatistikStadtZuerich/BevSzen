ff070dacac36f2fc9b5add9530acf5d2278022e8
sszrok <klemens.rosin@zuerich.ch>
2024-05-17 16:10:01 +0200

    model control flow: initial

--- 1_Code/0000_General/model_control-flow.r
+++ 1_Code/0000_General/model_control-flow.r
@@ -3,14 +3,14 @@ params <- init("middle_birth_lower")
 
 time_start <- Sys.time()
 
-run_scen(scenarios = c("middle", "middle_birth_lower", "middle_birth_upper"), 
-         modules = c("alw"))
-
-Sys.time() - time_start
+# run_scen(scenarios = c("middle", "middle_birth_lower", "middle_birth_upper"), 
+#          modules = c("alw"))
 
 # run_scen(
 #     scenarios = c("lower", "middle", "middle_birth_lower", "middle_birth_upper", "upper"),
 #     modules = c("all"))
+
+Sys.time() - time_start
 
 
 render_book(cache_refresh = TRUE)
