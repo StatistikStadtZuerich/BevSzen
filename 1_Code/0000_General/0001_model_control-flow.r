559c2ff6783336d6c9358c4831c89a412d63ac99
sszrok <klemens.rosin@zuerich.ch>
2024-05-29 11:29:00 +0200

    model control flow

--- 1_Code/0000_General/0001_model_control-flow.r
+++ 1_Code/0000_General/0001_model_control-flow.r
@@ -1,18 +1,18 @@
 source("1_code/0000_general/general_init.R")
-params <- init("middle") # inits assign directly to global env > doenst need to be
+params <- init("middle") 
 
 time_start <- Sys.time()
 
-run_scen(scenarios = c("middle"), modules = c("hou"))
-
-run_scen(scenarios = c("middle"), modules = c("car"))
-run_scen(scenarios = c("middle"), modules = c("spa"))
-run_scen(scenarios = c("middle"), modules = c("aca"))
-run_scen(scenarios = c("middle"), modules = c("pro"))
-run_scen(scenarios = c("middle"), modules = c("own"))
+# run_scen(
+#     scenarios = c("lower", "middle_birth_lower", "middle_birth_upper", middle", "upper"),
+#     modules = c("alw"))
 
 # run_scen(
-#     scenarios = c("lower", "middle", "upper"),
-#     modules = c("out"))
+#     scenarios = c("lower", "middle_birth_lower", "middle_birth_upper", middle", "upper"),
+#     modules = c("all"))
+
+Sys.time() - time_start
+
 
 render_book(cache_refresh = TRUE)
+
