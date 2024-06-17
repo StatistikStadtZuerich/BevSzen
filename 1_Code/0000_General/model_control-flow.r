6e87c025ed42db320237aedc85265906d8af8eab
sszrok <klemens.rosin@zuerich.ch>
2024-06-17 10:56:58 +0200

    new parameter sets (projects)

--- 1_Code/0000_General/model_control-flow.r
+++ 1_Code/0000_General/model_control-flow.r
@@ -3,15 +3,19 @@ params <- init("middle")
 
 time_start <- Sys.time()
 
-# run_scen(
-#     scenarios = c("lower", "middle", "middle_birth_lower", "middle_birth_upper", "upper"),
-#     modules = c("alw"))
+run_scen(scenarios = c("lower", "middle", "upper"), modules = c("pro"))
+run_scen(scenarios = c("lower", "middle", "upper"), modules = c("hou"))
+run_scen(scenarios = c("lower", "middle", "upper"), modules = c("deh"))
+run_scen(scenarios = c("lower", "middle", "upper"), modules = c("out"))
+
 
 # run_scen(
 #     scenarios = c("lower", "middle", "middle_birth_lower", "middle_birth_upper", "upper"),
 #     modules = c("all"))
 
 Sys.time() - time_start
+
+
 
 render_book(cache_refresh = TRUE)
 
