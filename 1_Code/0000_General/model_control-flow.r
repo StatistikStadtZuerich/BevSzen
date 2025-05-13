8dc98b8777cf17ba6f67ed69557aad580a1a9c7b
sszrok <klemens.rosin@zuerich.ch>
2025-05-13 10:56:02 +0200

    parameter, run 2025, demography

--- 1_Code/0000_General/model_control-flow.r
+++ 1_Code/0000_General/model_control-flow.r
@@ -3,9 +3,9 @@ params <- init("middle")
 
 time_start <- Sys.time()
 
-# run_scen(
-#     scenarios = c("lower", "middle", "middle_birth_lower", "middle_birth_upper", "upper"),
-#     modules = c("alw"))
+run_scen(
+    scenarios = c("middle"),
+    modules = c("aca"))
 
 # run_scen(
 #     scenarios = c("lower", "middle", "middle_birth_lower", "middle_birth_upper", "upper"),
